38fc141bb6506c433bd21a035a3c5676cf078e08
ekiN <nike275mm@gmail.com>
2020-06-02 10:01:55 +0900

    Step06 added

--- Step06_Constructor/src/test/main/MainClass01.java
+++ Step06_Constructor/src/test/main/MainClass01.java
@@ -7,5 +7,6 @@ public class MainClass01 {
 		// Student 객체를 생성해서 참조값을 s1이라는 변수에 담
 		
 		Student s1 = new Student();
+		Student s2 = new Student();
 	}
 }
---
+++ Step06_Constructor/src/test/main/MainClass04.java
@@ -0,0 +1,9 @@
+package test.main;
+
+import java.util.Random;
+
+public class MainClass04 {
+	public static void main(String[] args) {
+		new Random();
+	}
+}
