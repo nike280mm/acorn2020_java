210effff12b687a65533117c2b24accc7d0765d2
ekiN <nike275mm@gmail.com>
2020-06-02 14:57:25 +0900

    Step07 added

--- Step07_WrapperClass/src/test/main/MainClass01.java
+++ Step07_WrapperClass/src/test/main/MainClass01.java
@@ -19,5 +19,18 @@ public class MainClass01 {
 		int num1 = 10;
 		// 참조 데이터 type
 		Integer num2 = 10;
+		
+		// 참조 데이터 type이지만 기본 데이터 type 처럼 사용할 수 있다
+		int result1 = num2 + 1;
+		int result2 = num1 + num2;
+		Integer result3 = num1 + num2;
+		
+		// 숫자를 문자로 바꾸기 10 => "10"
+		String strNum = num2.toString();
+		
+		// 문자를 숫자로 바꾸기
+		String strNum2 = "999";		
+		
+		int result4 = Integer.parseInt(strNum2);
 	}
 }
