70f13d42275541d8f6849df76aefc932fc80e315
ekiN <nike275mm@gmail.com>
2020-06-02 12:37:51 +0900

    Project added

---
+++ Step07_WrapperClass/src/test/main/MainClass01.java
@@ -0,0 +1,23 @@
+package test.main;
+/*
+ * [ 기본 data type의 참조 data type ]
+ * 
+ * byte		: Byte
+ * short	: Short
+ * int		: integer
+ * long		: Long
+ * 
+ * float	: Float
+ * double	: Double
+ * 
+ * char		: Character
+ * boolean	: Boolean
+ */
+public class MainClass01 {
+	public static void main(String[] args) {
+		// 기본 데이터 type
+		int num1 = 10;
+		// 참조 데이터 type
+		Integer num2 = 10;
+	}
+}
